04007d58189a5919f90dd161204aff2b8b38db86
vmaguiar <vmaguiar@hotmail.com>
2023-07-20 15:45:41 -0300

    feat: creates colors for status

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -1,4 +1,4 @@
-import { HistoryContainer, HistoryList } from "./styles";
+import { HistoryContainer, HistoryList, Status } from "./styles";
 
 export function History() {
   return (
@@ -21,21 +21,33 @@ export function History() {
               <td>Qual tarefa</td>
               <td>20 minutos</td>
               <td>Há 2 meses</td>
-              <td>Concluído</td>
+              <td>
+                <Status statusColor="yellow">
+                  Em andamento
+                </Status>
+              </td>
             </tr>
 
             <tr>
               <td>Qual tarefa 2</td>
               <td>15 minutos</td>
               <td>Há 2 meses</td>
-              <td>Concluído</td>
+              <td>
+                <Status statusColor="green">
+                  Concluído
+                </Status>
+              </td>
             </tr>
 
             <tr>
               <td>Qual tarefa 3</td>
               <td>10 minutos</td>
               <td>Há 2 meses</td>
-              <td>Concluído</td>
+              <td>
+                <Status statusColor="red">
+                  interrompido
+                </Status>
+              </td>
             </tr>
           </tbody>
         </table>
