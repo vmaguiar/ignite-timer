7959f69291a7ca94f41728d4b89261489b24ad6f
vmaguiar <vmaguiar@hotmail.com>
2023-07-21 11:43:48 -0300

    feat: uses zod to create validation schema

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,12 +1,28 @@
 import { Play } from "phosphor-react";
 import { useForm } from 'react-hook-form'
+import { zodResolver } from "@hookform/resolvers/zod";
+import * as z from 'zod'
 
 import { FormContainer, HomeContainer, CountDownContainer, Separator, StartCountDownButton,
   TaskInput, AmountOfMinutesInput } from "./styles";
 
 
+  const newCycleFormValidationSchema = z.object({
+    task: z.string().min(1, 'Informe a tarefa'),
+    minutesAmount: z.number().min(5, 'O ciclo deve ser no minimo 5 min')
+    .max(60, 'O cilo precisa ser no máximo 60 min'),
+  })
+
+  type NewCyleFormData = z.infer<typeof newCycleFormValidationSchema>
+
 export function Home() {
-  const { register, handleSubmit, watch } = useForm()
+  const { register, handleSubmit, watch } = useForm<NewCyleFormData>({
+    resolver: zodResolver(newCycleFormValidationSchema),
+    defaultValues: {
+      task: '',
+      minutesAmount: 0
+    }
+  })
 
 
   const task = watch('task')
